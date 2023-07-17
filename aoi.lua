6d3a35054e2f59488d9e847e422c15a8a7060295
sigilwd <136939217+sigilwd@users.noreply.github.com>
2023-07-16 23:06:37 -0400

    Create aoi.lua

---
+++ aoi.lua
@@ -0,0 +1,15 @@
+--[[
+    KongerHub
+]]
+
+local gameLinks = {
+    [338574920] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/3plane.lua",
+    [9924209123] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/3plane.lua",
+    [1962086868] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/toh.lua",
+}
+
+if gameLinks[game.PlaceId] ~= nil then
+    loadstring(game:HttpGet(gameLinks[game.PlaceId]))()
+else
+    warn("KongerHub isn't supported in this game")
+end
