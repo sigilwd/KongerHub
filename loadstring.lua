c5f23b5f9c4af62315dbc01ce580d988e358e111
sigilwd <136939217+sigilwd@users.noreply.github.com>
2023-07-17 12:56:46 -0400

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,8 @@
+--[[
+KongerHub currently supports five games.
+1. Wings of glory
+2. Aviation Airways
+3. Tower of Hell
+4. Murderer VS Sheriff Duels
+]]--
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sigilwd/KongerHub/main/aoi.lua"))()
