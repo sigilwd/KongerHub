70e2d89a8eed28d8cf9074e326240dd6a9e65038
sigilwd <136939217+sigilwd@users.noreply.github.com>
2023-07-19 17:45:29 -0400

    Update aoi.lua

--- aoi.lua
+++ aoi.lua
@@ -3,8 +3,6 @@
 ]]
 
 local gameLinks = {
-    [338574920] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/3plane.lua",
-    [9924209123] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/3plane.lua",
     [1962086868] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/toh.lua",
     [12355337193] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/mvsd.lua",
 }
