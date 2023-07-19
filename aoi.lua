0742cce4d07724efdac47b4b8b671a03b20d2ae5
sigilwd <136939217+sigilwd@users.noreply.github.com>
2023-07-19 17:56:18 -0400

    Update aoi.lua

--- aoi.lua
+++ aoi.lua
@@ -1,7 +1,7 @@
 --[[
     KongerHub
 ]]
-
+setfpscap(10000)
 local gameLinks = {
     [1962086868] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/toh.lua",
     [12355337193] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/mvsd.lua",
