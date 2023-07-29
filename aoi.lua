f09d242a143c4a8980e9ee053df872dabca8223b
sigilwd <136939217+sigilwd@users.noreply.github.com>
2023-07-28 22:59:53 -0400

    Update aoi.lua

--- aoi.lua
+++ aoi.lua
@@ -5,6 +5,7 @@ setfpscap(10000)
 local gameLinks = {
     [1962086868] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/toh.lua",
     [12355337193] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/mvsd.lua",
+    [3214114884] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/flagwars",
 }
 
 if gameLinks[game.PlaceId] ~= nil then
