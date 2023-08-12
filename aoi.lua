f1940488c8114801d95437cd33d74160c3aec6d2
sigilwd <136939217+sigilwd@users.noreply.github.com>
2023-08-12 13:27:35 -0400

    Update aoi.lua

--- aoi.lua
+++ aoi.lua
@@ -6,7 +6,8 @@ local gameLinks = {
     [1962086868] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/toh.lua",
     [12355337193] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/mvsd.lua",
     [3214114884] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/flagwars.lua",
-    [6722284015] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/pistol1v1",
+    [6722284015] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/pistol1v1.lua",
+    [4410049285] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/drivingsimulator.lua",
 }
 
 if gameLinks[game.PlaceId] ~= nil then
