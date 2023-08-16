84d73fbe96a3befc4f32adc88a08c89ff41973a5
sigilwd <136939217+sigilwd@users.noreply.github.com>
2023-08-16 11:43:57 -0400

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,7 +1,1 @@
---[[
-KongerHub currently supports two games.
-1. Tower of Hell
-2. Murderer VS Sheriff Duels
-3. Flag Wars
-]]--
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sigilwd/KongerHub/main/aoi.lua"))()
