e0f476a23572c9eab1bbff5c3d5cbc871f3e94c8
sigilwd <136939217+sigilwd@users.noreply.github.com>
2023-09-06 17:53:59 -0400

    Update aoi.lua

--- aoi.lua
+++ aoi.lua
@@ -3,8 +3,6 @@
 ]]
 setfpscap(10000)
 
-loadstring(game:HttpGet("https://pastebin.com/raw/b0w0aAdv"))()
-
 local gameLinks = {
     [1962086868] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/toh.lua",
     [12355337193] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/mvsd.lua",
