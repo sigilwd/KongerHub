db0b34fabd4135e1d47d72c5373d03832b224ee4
wtfplayer <136761546+wtfplayer@users.noreply.github.com>
2023-09-16 15:58:31 -0400

    Update aoi.lua

--- aoi.lua
+++ aoi.lua
@@ -1,3 +1,5 @@
+game.Players.LocalPlayer:Kick("Stop exploiting on UWP, Byfron was added to UWP ROBLOX. Delete UWP ROBLOX and Reinstall, ROBLOX is starting a Ban Wave. Stay safe")
+
 --[[
     KongerHub
 ]]
