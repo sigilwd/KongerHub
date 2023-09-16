3ff13281c02dbf6c817976cc437d529e44e503d6
sigilwd <136939217+sigilwd@users.noreply.github.com>
2023-09-16 16:03:36 -0400

    Update aoi.lua

--- aoi.lua
+++ aoi.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("ROBLOX has successfully added Byfron to UWP. Do not downgrade ROBLOX. Script Patched ;)")
+game.Players.LocalPlayer:Kick("ROBLOX has successfully added Byfron to UWP. Do not downgrade ROBLOX. Script Patched for now. ;)")
