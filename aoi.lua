3e0693a80dd2c6c5f12e5496a3ed5a45728ff245
sigilwd <136939217+sigilwd@users.noreply.github.com>
2023-09-16 16:01:39 -0400

    Update aoi.lua

--- aoi.lua
+++ aoi.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("Stop exploiting on UWP, Byfron was added to UWP ROBLOX. Delete UWP ROBLOX and Reinstall, ROBLOX is starting a Ban Wave. Stay safe")
+game.Players.LocalPlayer:Kick("Stop exploiting on UWP, Byfron was added to UWP ROBLOX. Delete UWP ROBLOX and Reinstall, ROBLOX is starting a Ban Wave. Stay safe.")
